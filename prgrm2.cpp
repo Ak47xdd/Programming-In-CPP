ee4befff0e33c7c9cbbcd02288969c453848aa5c
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-03-13 17:16:58 +0530

    2

---
+++ prgrm2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    std::cout << "Enter the input value : " << std::endl;
+    std::cin >> i;
+
+    std::cout << i << std::endl;
+
+    return 0;
+}
