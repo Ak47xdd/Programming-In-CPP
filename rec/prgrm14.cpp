74ef719dd07b10ece7a75e10a3d1a3b3a1bb6c5f
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-03-26 16:27:28 +0530

    del

---
+++ rec/prgrm14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int number;
+    int sum = 0;
+
+    std::cout << "Enter a number : " << std::endl;
+    std::cin >> number;
+
+    while (number > 0)
+    {
+        std::cout << "Enter a number : " << std::endl;
+        std::cin >> number;
+        sum += number;
+    }
+    std::cout << "The sum is : " << sum << std::endl;
+
+    return 0;
+}
