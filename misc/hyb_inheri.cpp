3e591fecefdae8c6539f58f81ffd05a9de9c24dc
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-03-31 16:00:45 +0530

    add

---
+++ misc/hyb_inheri.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class A
+{
+    // body
+};
+class B
+{
+    // body
+};
+class C : public B
+{
+    // body
+};
+class D : public C
+{
+    // body
+};
+class E : public C
+{
+    // body
+};
