9cb474810c5360da99c95f24f32050b46f0919f1
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-01 16:35:50 +0530

    add

---
+++ misc/oprt_over_unary.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+class Distance
+{
+private:
+  int feet, inches;
+
+public:
+  Distance(int f, int i)
+  {
+    feet = f;
+    inches = i;
+  }
+  void display()
+  {
+    std::cout << "Feet : " << feet << "\n"
+              << "Inches : " << inches << std::endl;
+  }
+  Distance operator-()
+  {
+    return Distance(-feet, -inches);
+  }
+};
+int main()
+{
+  Distance D1(10, -11);
+  -D1;
+  D1.display();
+
+  return 0;
+}
