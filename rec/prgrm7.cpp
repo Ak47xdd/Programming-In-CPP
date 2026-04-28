efd871a1274c72ecebbc110aa5f596b9774863f9
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-28 21:59:15 +0530

    prgrm7

--- rec/prgrm7.cpp
+++ rec/prgrm7.cpp
@@ -2,12 +2,26 @@
 
 int main(int argc, char *argv[])
 {
-    int a = 2, b = 3;
-    std::cout << "Before a = " << a << "b = " << b << std::endl;
-    int temp = a;
-    a = b;
-    b = temp;
-    std::cout << "After a = " << a << "b = " << b << std::endl;
+    int n, num, digit, rev = 0;
+
+    std::cout << "Enter a positive number : " << std::endl;
+    std::cin >> num;
+
+    n = num;
+
+    do
+    {
+        digit = num % 10;
+        rev = (rev * 10) + digit;
+        num = num / 10;
+    } while (num != 0);
+
+    std::cout << "The reverse of the number is : " << rev << std::endl;
+
+    if (n == rev and n > 0)
+        std::cout << "The number is a palindrome" << std::endl;
+    else
+        std::cout << "The number is not a palindrome" << std::endl;
 
     return 0;
 }
