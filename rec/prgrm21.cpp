c8732d857402cd1976387cc2ef9e431661ff11b9
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-30 10:32:43 +0530

    prgrm21

--- rec/prgrm21.cpp
+++ rec/prgrm21.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
+using namespace std;
 
-int fact(int n)
+class Complex
 {
-    if (n < 0)
-    {
-        std::cout << "Error: Factorial is not defined for negative numbers." << std::endl;
-        return -1; // Return -1 to indicate an error
-    }
-    else if (n == 0 || n == 1)
-    {
-        return 1; // Factorial of 0 and 1 is 1
-    }
-    else
-    {
-        return n * fact(n - 1); // Recursive call
-    }
-}
+private:
+    float real, imag;
 
-int main(int argc, char *argv[])
+public:
+    Complex()
+    {
+        real = 0;
+        imag = 0;
+    }
+    Complex(float r, float i)
+    {
+        real = r;
+        imag = i;
+    }
+    Complex operator+(Complex obj)
+    {
+        Complex temp;
+        temp.real = real + obj.real;
+        temp.imag = imag + obj.imag;
+
+        return temp;
+    }
+    void display()
+    {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main()
 {
-    int number;
+    Complex C1(2, 3), C2(4, 5);
+    Complex C3 = C1 + C2;
 
-    std::cout << "Enter a positive integer : ";
-    std::cin >> number;
+    cout << "Result : " << endl;
 
-    int result = fact(number);
-    if (result != -1)
-    {
-        std::cout << "Factorial of " << number << " = " << result << std::endl;
-    }
+    C3.display();
 
     return 0;
 }
