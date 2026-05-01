e4aca51edc1c4a43e7a231694fb584d09d42fbe4
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-05-01 15:21:41 +0530

    prgrm24

--- rec/prgrm24.cpp
+++ rec/prgrm24.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-class Reactangle
+int main()
 {
-private:
-    float length, breadth;
-    double area;
+    string str1 = "Hello";
+    string str2 = "World";
+    string result = str1 + " " + str2;
 
-public:
-    void get(int l, int b)
-    {
-        length = l;
-        breadth = b;
-        area = length * breadth;
-    }
-    void display()
-    {
-        std::cout << "Length : " << length << std::endl;
-        std::cout << "Breadth : " << breadth << std::endl;
-        std::cout << "Area : " << area << std::endl;
-    }
-};
+    cout << "Concatenation : " << result << endl;
+    cout << "Length : " << result.length() << endl;
 
-int main(int argc, char *argv[])
-{
-    Reactangle r1, r2;
-    std::cout << "Rectangle 1 : " << std::endl;
-    r1.get(10.5, 7.5);
-    r1.display();
-    std::cout << "*************************" << std::endl;
-    std::cout << "Rectangle 2 : " << std::endl;
-    r2.get(25.0, 12.5);
-    r2.display();
-    std::cout << "**************************" << std::endl;
+    if (result.find("World") != std::string::npos)
+        cout << "Substring found" << endl;
+
+    result.replace(6, 5, "C++");
+    cout << "After result : " << result << endl;
+    cout << "Charaters in given string : ";
+
+    for (int i = 0; i < result.length(); i++)
+    {
+        cout << result[i] << "";
+    }
+
     return 0;
 }
